bc878bd454433200f0d239f1df23d69b833e1a57
izcomayana <oluwapelumisotoyinbo@gmail.com>
2026-07-11 12:21:27 +0100

    reminder fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,14 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <>
       <Toaster richColors position="bottom-right" />
-      <body className="min-h-full flex flex-col">
-        <AuthProvider>{children}</AuthProvider>
-      </body>
-    </html>
+      <html
+        lang="en"
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">
+          <AuthProvider>{children}</AuthProvider>
+        </body>
+      </html>
+    </>
   );
 }
